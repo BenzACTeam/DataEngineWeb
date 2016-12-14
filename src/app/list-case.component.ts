eb05014592e3baa5b527a3714b2a85e447ff9e78
dubigouxiong <dubigouxiong@qq.com>
2016-12-14 10:04:03 +0800

    test

--- src/app/list-case.component.ts
+++ src/app/list-case.component.ts
@@ -3,7 +3,7 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { Site } from './site';
+// import { Site } from './site';
 
 @Component({
   selector: 'content',
@@ -11,52 +11,52 @@ import { Site } from './site';
 })
 export class ListCaseComponent {
 
-  firstName: string;
-  constructor(){
-    this.firstName="2b";
-  }
-  // doSomething($event){
+  // firstName: string;
+  // constructor(){
+  //   this.firstName="2b";
+  // }
+  // // doSomething($event){
+  // //
+  // //   console.log('点击了这个按钮：',$event.target.value);
+  // // }
   //
-  //   console.log('点击了这个按钮：',$event.target.value);
+  // getVal(){
+  //   return 1;
   // }
-
-  getVal(){
-    return 1;
-  }
-  title = '站点列表';
-  sites = ['菜鸟教程', 'Google', 'Taobao', 'Facebook'];
-  mySite = this.sites[0];
-
-  clickMessage = '';
-
-  onClickMe() {
-    this.clickMessage = '菜鸟教程!';
-  }
-
-
-  values = '';
-   // 非强类型
-   onKey(event:any) {
-   this.values = event.target.value + ' | ';
-   }
-
-  // 强类型
-  // onKey(event: KeyboardEvent) {
-  //   this.values += (<HTMLInputElement>event.target).value + ' | ';
+  // title = '站点列表';
+  // sites = ['菜鸟教程', 'Google', 'Taobao', 'Facebook'];
+  // mySite = this.sites[0];
+  //
+  // clickMessage = '';
+  //
+  // onClickMe() {
+  //   this.clickMessage = '菜鸟教程!';
   // }
-  urls = ['www.runoob.com', 'www.google.com',
-    'www.taobao.com', 'www.facebook.com'];
-  model = new Site(1, '菜鸟教程', this.urls[0], 10000);
-  submitted = false;
-  onSubmit() { this.submitted = true; }
-  // TODO: 完成后移除
-  get diagnostic() { return JSON.stringify(this.model); }
-
-  active = true;
-  newSite() {
-    this.model = new Site(5, '', '');
-    this.active = false;
-    setTimeout(() => this.active = true, 0);
-  }
+  //
+  //
+  // values = '';
+  //  // 非强类型
+  //  onKey(event:any) {
+  //  this.values = event.target.value + ' | ';
+  //  }
+  //
+  // // 强类型
+  // // onKey(event: KeyboardEvent) {
+  // //   this.values += (<HTMLInputElement>event.target).value + ' | ';
+  // // }
+  // urls = ['www.runoob.com', 'www.google.com',
+  //   'www.taobao.com', 'www.facebook.com'];
+  // model = new Site(1, '菜鸟教程', this.urls[0], 10000);
+  // submitted = false;
+  // onSubmit() { this.submitted = true; }
+  // // TODO: 完成后移除
+  // get diagnostic() { return JSON.stringify(this.model); }
+  //
+  // active = true;
+  // newSite() {
+  //   this.model = new Site(5, '', '');
+  //   this.active = false;
+  //   setTimeout(() => this.active = true, 0);
+  // }
 
 }

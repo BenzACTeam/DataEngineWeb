3226e74d83b57274ea6146eba04e5f09a5836993
polluxliu <pollux.liu@msn.com>
2016-12-14 14:56:07 +0800

    optimize dashboard page

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,12 +2,12 @@
  * Created by paulliu on 2016/12/7.
  */
 
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'content',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
-
+  cases: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }

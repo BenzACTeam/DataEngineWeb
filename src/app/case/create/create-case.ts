8a46605abcfd18f820276da1ceae094a68df53fd
dubigouxiong <dubigouxiong@qq.com>
2016-12-19 16:02:17 +0800

    html.post

---
+++ src/app/case/create/create-case.ts
@@ -0,0 +1,26 @@
+
+
+export class CreateCase {
+  constructor(
+
+
+    public subject:string,
+  public value:string,
+  public scenario:string,
+  public result:string,
+  public dataSource: string,
+  public dataTypeDescription: string,
+  public analysisModel: string,
+  public contributionBU: string,
+  public stage:string,
+  public fromDate:string,
+  public toDate:string,
+  public contact:string,
+  public help:string,
+  public analysisMethodDescrption:string
+  ) {}
+
+
+
+}
+

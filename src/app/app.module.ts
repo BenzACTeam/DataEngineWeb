e16db030b26be48a79909ff0a75659997d19b920
dubigouxiong <dubigouxiong@qq.com>
2016-12-30 16:27:35 +0800

    user css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HttpModule} from '@angular/http';
 
 import {AppRoutingModule}     from './app-routing.module';
 import {SelectModule}  from 'ng2-select';
-import {ProgressbarModule} from 'ng2-bootstrap';
+import {ProgressbarModule, TabsModule} from 'ng2-bootstrap';
 import {AccordionModule} from 'ng2-accordion';
 import {FileUploadModule} from '../../node_modules/ng2-file-upload/components/file-upload/file-upload.module';
 import {Ng2DatetimePickerModule} from 'ng2-datetime-picker';
@@ -19,6 +19,8 @@ import {TestComponent} from "./test/test";
 import {EditCaseComponent} from "./case/edit/edit-case.component";
 import {UserComponent} from "./user/user.component";
 import {ListSearchComponent} from "./search/list-search.component";
+import {UserMoreComponent} from "./user/more/user-more.component";
+
 
 import {HttpHelper} from "./http-helper";
 import {UseCaseService} from './case/use-case.service';
@@ -35,6 +37,7 @@ import {UseCaseService} from './case/use-case.service';
     EditCaseComponent,
     UserComponent,
     ListSearchComponent,
+    UserMoreComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +48,8 @@ import {UseCaseService} from './case/use-case.service';
     SelectModule,
     Ng2DatetimePickerModule,
     ProgressbarModule,
-    AccordionModule
+    AccordionModule,
+    TabsModule
   ],
   providers: [HttpHelper, UseCaseService],
   bootstrap: [AppComponent]

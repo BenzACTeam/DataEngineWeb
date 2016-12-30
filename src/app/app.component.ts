e4dd8cea2da86e40ab807ab6f97a53e2b868ff1b
polluxliu <pollux.liu@msn.com>
2016-12-30 16:57:52 +0800

    optimize default page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,29 @@
-import { Component } from '@angular/core';
-import { Site } from './menu';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  menu = [
-    new Site(1, 'Home',"dashboard"),
-    new Site(1, 'Case List',"listcase"),
-    new Site(2, 'Create Case',"createcase"),
-    new Site(2, 'Detail',"detail"),
-    new Site(2, 'Test',"test")
-  ];
 
-  mySite = this.menu[0];
+  private showSidebar = true;
+  private sidebarStyle;
+  private mainContentStyle;
 
-  constructor() { }
+  private toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
 
-  public disabled:boolean = false;
-  public status:{isopen:boolean} = {isopen: false};
-
-  public toggled(open:boolean):void {
-    console.log('Dropdown is now: ', open);
-  }
-
-  public toggleDropdown($event:MouseEvent):void {
-    $event.preventDefault();
-    $event.stopPropagation();
-    this.status.isopen = !this.status.isopen;
+    if (!this.showSidebar) {
+      this.sidebarStyle = {
+        'margin-left': '-240px'
+      };
+      this.mainContentStyle = {
+        'margin-left': '0'
+      };
+    }
+    else {
+      this.sidebarStyle = {};
+      this.mainContentStyle = {};
+    }
   }
 }
